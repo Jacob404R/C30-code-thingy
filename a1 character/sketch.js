b91b227c399cb360a412f7336e3e25aed487bfad
Jacob404R <franzfranc311@gmail.com>
2024-02-29 11:50:25 -0600

    first project

---
+++ a1 character/sketch.js
@@ -0,0 +1,22 @@
+// A1 Character
+// Jacob Rivera
+// 29 Feb 2024
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  fill("red");
+  rect(width/4,height/4, 400, 400);
+  fill("white");
+  rect(width/4+400,height/4, 400, 400);
+  fill("blue");
+  circle(width/2, height/2, 100);
+
+}
