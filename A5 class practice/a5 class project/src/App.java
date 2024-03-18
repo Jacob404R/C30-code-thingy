41b4b2b6634df70f3188b4dd4efb0559df62ac24
Jacob404R <franzfranc311@gmail.com>
2024-03-18 11:25:09 -0600

    a5thing

---
+++ A5 class practice/a5 class project/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to CS 30 \n\tMy name is Jacob\n\t\tMy favourite food is cardboard");
+    }
+}
