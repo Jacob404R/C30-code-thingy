0c1a7ab40fff56130ee2230577d9f77e9258d14f
Jacob404R <franzfranc311@gmail.com>
2024-03-19 11:54:50 -0600

    a6 variables

--- A6 variables/a6 variables/src/App.java
+++ A6 variables/a6 variables/src/App.java
@@ -1,5 +1,17 @@
+/*Program name: A6 Variables
+
+Student name: Jacob Rivera
+
+Date: 19 March 2024
+
+Program Description: Calculates chicken wings */
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double money = 600;
+        double cost = 0.75;
+        double wings = money / cost;
+        System.out.println("You can buy " + (int) wings + " Chicken wings with $" + money);
+
     }
 }
