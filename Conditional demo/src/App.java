6e182d573402f67dc6f11f501daa2ce04fe9f8c0
Jacob404R <franzfranc311@gmail.com>
2024-04-08 11:19:40 -0600

    demo

---
+++ Conditional demo/src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // int x = 0;
+
+        // if (x>5){
+        //     System.out. println("Big Number");
+        // }
+        // else if (x<-12){
+        //     System.out.println("Mild Weather");
+        // }
+        // else{
+        //     System.out.println("Cold");
+        // }
+
+        boolean isCold = true;
+
+        if (isCold){
+            System.out.println("Wear Gloves");
+        }
+    }
+}
