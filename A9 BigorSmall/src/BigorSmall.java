eeacf2e6e0ea902b135b81b1faea6012e8511698
Jacob404R <franzfranc311@gmail.com>
2024-04-08 11:25:45 -0600

    a9

---
+++ A9 BigorSmall/src/BigorSmall.java
@@ -0,0 +1,7 @@
+public class BigorSmall {
+    public void numCheck(){
+        
+    
+        
+    }
+}
---
+++ A9 BigorSmall/src/BigorSmallrunner.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class BigorSmallrunner {
+    public static void main(String[] args) throws Exception {
+        Scanner num = new Scanner(System.in);
+        System.out.println("Please enter 2 numbers");
+
+        Integer num1 = num.nextInt();
+        Integer void = num.nextInt();
+        Integer num2 = num.nextInt();
+    }
+}
