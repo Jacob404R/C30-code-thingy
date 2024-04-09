f099e037fbd72ff2f213f8a7ab6e7a3948b30403
Jacob404R <franzfranc311@gmail.com>
2024-04-09 11:23:44 -0600

    finished assignment

--- A9 BigorSmall/src/BigorSmall.java
+++ A9 BigorSmall/src/BigorSmall.java
@@ -1,7 +1,15 @@
 public class BigorSmall {
-    public void numCheck(){
-        
-    
+    public void numCheck(int numa, int numb){ 
+        System.out.println("Is " + numa + " bigger than " + numb + "?");
+        if (numa > numb){
+            System.out.println("Yes");
+        }
+        else if (numa < numb){
+            System.out.println("No");
+        }
+        else{
+            System.out.println("Same number");
+        }
         
     }
 }
--- A9 BigorSmall/src/BigorSmallrunner.java
+++ A9 BigorSmall/src/BigorSmallrunner.java
@@ -2,10 +2,33 @@ import java.util.Scanner;
 public class BigorSmallrunner {
     public static void main(String[] args) throws Exception {
         Scanner num = new Scanner(System.in);
-        System.out.println("Please enter 2 numbers");
+        
+        BigorSmall numbers = new BigorSmall();
+        Boolean trigger = true;
+        while (trigger){
 
-        Integer num1 = num.nextInt();
-        Integer void = num.nextInt();
-        Integer num2 = num.nextInt();
+            System.out.println("Please enter 2 numbers");
+
+            Integer num1 = num.nextInt();
+
+            Integer num2 = num.nextInt();
+
+            numbers.numCheck(num1,num2);
+
+            System.out.println("Try again? (1)yes / (0)no");
+
+            Integer yn = num.nextInt();
+
+            if (yn == 0){
+                trigger = !trigger;
+            }
+            else if (yn == 1){
+                trigger = true;
+            }
+            else {
+                System.out.println("That aint one of the options");
+            }
+        }
+
     }
 }
