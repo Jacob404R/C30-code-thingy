42eca4f9518a87542e1418975df88a6c000226d0
Jacob404R <franzfranc311@gmail.com>
2024-04-11 10:41:13 -0600

    finished assignment

--- A10 Sum up stuff/src/SumUp.java
+++ A10 Sum up stuff/src/SumUp.java
@@ -1,11 +1,11 @@
 public class SumUp {
-    public void Addnums(double num1, double num2){
+    public double Addnums(double num1, double num2){
         double n1 = num1;
         double n2 = num2;
-        double answer = n1 + n2;
+        return n1 + n2;
 
     }
-    public void PrintAnswer(double d1, double d2){
-        System.out.println("The numbers" + d1 + "and" + d2 + "add up to" + answer + "!"  );
+    public void PrintAnswer(double d1, double d2, double answer){
+        System.out.println("The numbers " + d1 + " and " + d2 + " add up to " + answer + "!"  );
     }
 }
--- A10 Sum up stuff/src/SumUpRunner.java
+++ A10 Sum up stuff/src/SumUpRunner.java
@@ -1,5 +1,13 @@
-public class App {
+import java.util.Scanner;
+public class SumUpRunner {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner keys = new Scanner(System.in);
+        SumUp adding = new SumUp();
+        System.out.println("Please enter Real numbers");
+        double n = keys.nextDouble();
+        double n2 = keys.nextDouble();
+
+        adding.PrintAnswer(n, n2, adding.Addnums(n, n2));
+
     }
 }
