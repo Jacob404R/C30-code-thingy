1195d05cb99b9b74d679784cb472218690689f45
Jacob404R <franzfranc311@gmail.com>
2024-04-12 10:55:57 -0600

    finished temp converter

--- A12 temp converter/src/TempCalc.java
+++ A12 temp converter/src/TempCalc.java
@@ -1,10 +1,11 @@
 public class TempCalc {
-    public void celtoFah(double f1){
-        double cel = (f1 -32) /9/5;
-        System.out.println(cel);
+    public void celtoFah(double c1){
+        double fah = c1*(9.0/5)+32;
+        System.out.println(c1 +  "°F is equal to " + fah + "°C");
     }
 
-    public void fahtoCel(double c1){
-
+    public void fahtoCel(double f1){
+        double cel = (f1-32)*(5.0/9);
+        System.out.println(f1 +  "°F is equal to " + cel + "°C");
     }
 }
--- A12 temp converter/src/TempRunner.java
+++ A12 temp converter/src/TempRunner.java
@@ -3,6 +3,25 @@ import java.util.Scanner;
 public class TempRunner {
     public static void main(String[] args) throws Exception {
       TempCalc heat = new TempCalc();
-      heat.celtoFah(32); 
+      Scanner keys = new Scanner(System.in);
+      
+      System.out.println("Do you want to convert from (1) Celsius to Fahrenheit or (2) Fahrenheit to Celsius?");
+
+      Integer answer = keys.nextInt();
+      if (answer == 1){
+        System.out.println("please enter the temperature in Celsius: ");
+        double temp = keys.nextDouble(); 
+
+        heat.celtoFah(temp); 
+      }
+      else if  (answer == 2){
+        System.out.println("please enter the temperature in Fahrenheit: ");
+        double temp = keys.nextDouble(); 
+
+        heat.fahtoCel(temp); 
+      }
+      else{
+        System.out.println("That wasn't 1 or 2.");
+      }
     }
 }
