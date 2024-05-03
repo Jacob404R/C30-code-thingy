1d258a374408513af69fb086616fb027f9f4ebae
Jacob404R <franzfranc311@gmail.com>
2024-05-03 12:06:17 -0600

    pig rpg

--- Random test/src/App.java
+++ Random test/src/App.java
@@ -1,6 +1,14 @@
 import java.lang.Math;
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println(Math.round(Math.random()*21));
+        int x = 10;
+
+        int min = 10;
+        int max = 20-min;
+        while (x > 0){
+            System.out.println(Math.round(min+ Math.random()*max )); //5 to 10
+            x--;
+        }
     }
 }
